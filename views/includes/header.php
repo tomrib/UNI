3f5720115cabd1e28c77fdc4d26d86794b6ea776
tomrib <ribeiro.tommy.60@gmail.com>
2023-08-29 13:25:47 +0200

    MAJ

--- views/includes/header.php
+++ views/includes/header.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Entreprise UNI</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
 </head>
 
@@ -15,11 +18,35 @@
             <input type="checkbox" id="menu-toggle">
             <label for="menu-toggle" class="menu-icon"></label>
             <ul>
-                <li><a href="/bureau">Accueil</a></li>
-                <li><a href="/Liste-Employer">Employé(e)s</a></li>
-                <li><a href="/Liste-client">Client(e)s</a></li>
-                <li><a href="#">Archives</a></li>
-                <li><a href="#">Interventions</a></li>
+                <li><a href="/bureauAdministratrice">Accueil</a></li>
+
+                <li class="parent">
+                    <a href="#">Client(e)s</a>
+                    <ul class="sub-menu">
+                        <li id=><a href="#">Liste client(e)</a></li>
+                        <li><a href="#">Nouveau client(e)</a></li>
+                        <li><a href="#">Archives client(e)</a></li>
+                    </ul>
+                </li>
+                <li class="parent">
+                    <a href="#">Sous-traitant</a>
+                    <ul class="sub-menu">
+                        <li><a href="#">Liste sous-traitant</a></li>
+                        <li><a href="#">Nouveau sous-traitant</a></li>
+                        <li><a href="#">Archives sous-traitant</a></li>
+                    </ul>
+                </li>
+                <li class="parent">
+                    <a href="#  ">Employé(e)s</a>
+                    <ul class="sub-menu">
+                        <li><a href="#">Liste employé(e)</a></li>
+                        <li><a href="#">Nouveau employé(s)</a></li>
+                        <li><a href="#">Archives employé(e)</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Archives interventions</a>
+                </li>
                 <li class="login-modal-trigger"><a href="#">Se déconnecter</a></li>
             </ul>
         </nav>
