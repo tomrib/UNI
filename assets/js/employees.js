f2c8ce51192361ada4fc62b9d636194efeb3966d
Marc S <115186714+mapko-ST@users.noreply.github.com>
2023-09-07 16:13:53 +0200

    Maj 07.09 Marco

--- assets/js/employees.js
+++ assets/js/employees.js
@@ -1,71 +1,21 @@
-// Fonction pour ouvrir la modal en mode édition
-/*
-function openEditModal() {
-  editProfileButton.style.display = "inline-block";
-  deleteProfileButton.style.display = "inline-block";
-  openModal();
-}
-
-// Fonction pour ouvrir la modal
-/*function openModal() {
-  modal.classList.add("modal-visible");
-}
-
-// Fonction pour fermer la modal
-function closeModal() {
-  modal.classList.remove("modal-visible");
-  profileForm.reset();
-}
-
-// Écouteur d'événement pour le bouton de modification dans la modal
-editProfileButton.addEventListener("click", () => {
-  closeModal();
-  profileForm.submit();
-});
-
-// Écouteur d'événement pour le bouton de suppression dans la modal
-deleteProfileButton.addEventListener("click", () => {
-  closeModal();
-  deleteEmployee(currentEmployeeIndex);
-});
-*/
-// Écouteur d'événement pour fermer la modal en cliquant à l'extérieur
-/*window.addEventListener("click", (event) => {
-  if (event.target === modal) {
-    closeModal();
-  }
-});
-*/
-
-// Attendez que le DOM soit entièrement chargé avant d'exécuter le code
 document.addEventListener("DOMContentLoaded", function () {
-  // Récupérez tous les boutons "Info"
   var infoEmployee = document.querySelectorAll(".infoEmployee");
 
-  // Récupérez l'élément modal
-  var employeeModal = document.getElementById("employeeModal");
+  var displayModal = document.getElementById("displayModal");
 
-  // Récupérez le bouton de fermeture de la modal
   var closeModalButton = document.getElementById("closeModalButton");
 
-  // Ajoutez un gestionnaire d'événement à chaque bouton "Info"
   infoEmployee.forEach(function (button) {
     button.addEventListener("click", function (event) {
-      event.preventDefault(); // Empêchez la redirection par défaut
+      event.preventDefault();
 
-      // Récupérez l'URL du lien dans le bouton "Info"
       var editLink = this.parentElement.getAttribute("href");
 
-      // Vous pouvez maintenant charger les données de l'employé depuis l'URL
-      // et les afficher dans la modal (par exemple, en utilisant une requête AJAX)
-
-      // Affichez la modal
-      employeeModal.style.display = "block";
+      displayModal.style.display = "block";
     });
   });
 
-  // Ajoutez un gestionnaire d'événement pour fermer la modal
   closeModalButton.addEventListener("click", function () {
-    employeeModal.style.display = "none";
+    displayModal.style.display = "none";
   });
 });
--- views/employe/listEmploye.php
+++ views/employe/listEmploye.php
@@ -1,93 +1,96 @@
-<!-- Conteneur principal -->
-<div class="displayTable">
-    <h1>Liste des Employé(e)s</h1>
-    <div class="tableContainer">
-        <table id="employeesTable" class="responsiveTable">
-            <thead>
-                <tr>
-                    <th>Nom</th>
-                    <th>Prénom</th>
-                    <th>Email</th>
-                    <th>Téléphone</th>
-                    <th class="border" id="modalInfo" title="fiche d'information"><i class="fas fa-eye"></i></th>
-                    <th class="border"><i class="fas fa-edit" title="modifier la fiche"></i></th>
-                    <th class="border"><i class="fas fa-trash-alt" title="suppression de la fiche"></i></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($list as $list) { ?>
+    <!-- Conteneur principal -->
+    <div class="displayTable">
+        <h1>Liste des Employé(e)s</h1>
+        <div class="tableContainer">
+            <table id="employeesTable" class="responsiveTable">
+                <thead>
                     <tr>
-                        <td><?= $list->lastname ?></td>
-                        <td><?= $list->firstname ?></td>
-                        <td><?= $list->email ?></td>
-                        <td><?= $list->phone ?></td>
-
-                        <td class="border" id="modalInfo">
-                            <a href="./Liste-Employer-<?= $list->id ?>"><button class="infoEmployee" title="fiche d'information"><i class="fas fa-eye"></i></button></a>
-                        </td>
-                        <td class="border">
-                            <a href="./Modifier-Employer-<?= $list->id ?>"><button title="modifier la fiche"><i class="fas fa-edit"></i></button></a>
-                        </td>
-                        <td class="border">
-                            <form action="./Liste-Employer" id="deleteForm" method="POST">
-                                <input type="hidden" name="id_suppression" value="<?= $list->id ?>">
-                                <button type="submit" id="hoverDanger" name="delete" title="suppression de la fiche"><i class="fas fa-trash-alt"></i></button>
-                            </form>
-                        </td>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Email</th>
+                        <th>Téléphone</th>
+                        <th class="border" id="modalInfo" title="fiche d'information"><i class="fas fa-eye"></i></th>
+                        <th class="border"><i class="fas fa-edit" title="modifier la fiche"></i></th>
+                        <th class="border"><i class="fas fa-trash-alt" title="suppression de la fiche"></i></th>
                     </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
-    <!-- Bouton pour créer un nouveau profil -->
-    <button onclick="openModal()" class="create-profile-button"><a href="./Ajout-Employer">Créer un nouveau profil</a></button>
+                </thead>
+                <tbody>
+                    <?php foreach ($list as $list) { ?>
+                        <tr>
+                            <td class="fixedWidthColumn"><?= $list->lastname ?></td>
+                            <td class="fixedWidthColumn"><?= $list->firstname ?></td>
+                            <td class="fixedWidthColumn"><?= $list->email ?></td>
+                            <td class="fixedWidthColumn"><?= $list->phone ?></td>
 
-    <!--MODAL BUTTON-->
-    <div id="employeeModal" class="modal" style="display: none;">
-        <div class="modal-content">
+                            <td class="border" id="modalInfo">
+                                <a href="./Liste-Employer-<?= $list->id ?>"><button class="infoEmployee" title="fiche d'information"><i class="fas fa-eye"></i></button></a>
+                            </td>
+                            <td class="border">
+                                <a href="./Modifier-Employer-<?= $list->id ?>"><button title="modifier la fiche"><i class="fas fa-edit"></i></button></a>
+                            </td>
+                            <td class="border">
+                                <form action="./Liste-Employer" id="deleteForm" method="POST">
+                                    <input type="hidden" name="id_suppression" value="<?= $list->id ?>">
+                                    <button type="submit" id="hoverDanger" name="delete" title="suppression de la fiche"><i class="fas fa-trash-alt"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+        <!-- Bouton pour créer un nouveau profil -->
+        <button onclick="openModal()" class="create-profile-button"><a href="./Ajout-Employer">Créer un nouveau profil</a></button>
+
+        <!--MODAL BUTTON-->
+        <div id="displayModal" class="modal" style="display: none;">
             <h2>FICHE DE RENSEIGNEMENT</h2>
-            <div class="infoItem">
-                <p>Nom:</p>
-                <p></p>
+            <div class="modalContent">
+                <div class="infoItem">
+                    <p class="strongP">Nom:</p>
+                    <p></p>
+                </div>
+                <div class="infoItem">
+                    <p class="strongP">Prénom:</p>
+                    <p></p>
+                </div>
+                <div class="infoItem">
+                    <p class="strongP">Mail:</p>
+                    <p></p>
+                </div>
+                <div class="infoItem">
+                    <p class="strongP">Date de naissance:</p>
+                    <p></p>
+                </div>
+                <div class="infoItem">
+                    <p class="strongP">Adresse:</p>
+                    <p></p>
+                </div>
+                <div class="infoItem">
+                    <p class="strongP">Téléphone:</p>
+                    <p></p>
+                </div>
+                <div class="infoItem">
+                    <p class="strongP">Type de contrat:</p>
+                    <p></p>
+                </div>
+                <div class="infoItem">
+                    <p class="strongP">Date de début:</p>
+                    <p></p>
+                </div>
+                <div class="infoItem">
+                    <p class="strongP">Date de fin:</p>
+                    <p></p>
+                </div>
+                <div class="infoItem">
+                    <p class="strongP">Numéro de sécurité sociale:</p>
+                    <p></p>
+                </div>
             </div>
-            <div class="infoItem">
-                <p>Prénom:</p>
-                <p></p>
-            </div>
-            <div class="infoItem">
-                <p>Mail:</p>
-                <p></p>
-            </div>
-            <div class="infoItem">
-                <p>Date de naissance:</p>
-                <p></p>
-            </div>
-            <div class="infoItem">
-                <p>Adresse:</p>
-                <p></p>
-            </div>
-            <div class="infoItem">
-                <p>Téléphone:</p>
-                <p></p>
-            </div>
-            <div class="infoItem">
-                <p>Type de contrat:</p>
-                <p></p>
-            </div>
-            <div class="infoItem">
-                <p>Date de début:</p>
-                <p></p>
-            </div>
-            <div class="infoItem">
-                <p>Date de fin:</p>
-                <p></p>
-            </div>
-            <div class="infoItem">
-                <p>Numéro de sécurité sociale:</p>
-                <p></p>
+            <div class="buttonModal">
+                <button name="" id="downloadButton" type"submit"><i class="fas fa-download" title="télécharger la fiche"></i></button>
+                <button id="closeModalButton">Fermer</button>
             </div>
         </div>
-        <button id="closeModalButton">Fermer</button>
     </div>
-</div>
-<script src="assets/js/employees.js"></script>
+    <script src="assets/js/employees.js"></script>
