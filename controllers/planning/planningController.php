dbf8004a0e6d756513baa51b3618fe64feff2121
Tommy Ribeiro <115786099+tomrib@users.noreply.github.com>
2023-09-21 11:23:19 +0200

    mis a jour de uni

--- controllers/planning/planningController.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require_once '../../models/customersUserModel.php';
-require_once '../../models/userModel.php';
-require_once '../../models/custommersModel.php';
-require_once '../../confi.php';
-
